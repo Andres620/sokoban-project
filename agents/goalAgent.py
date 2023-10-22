30d87cc71935fda8e4b35b26185a3ea957dc306f
Andres620 <andres.1701625573@ucaldas.edu.co>
2023-10-22 16:55:12 -0500

    Implementadas funciones para leer archivos y cargar el mapa

---
+++ agents/goalAgent.py
@@ -0,0 +1,12 @@
+from mesa import Agent
+
+
+class GoalAgent(Agent):
+    def __init__(self,unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self) -> None:
+        pass
+
+    def move(self) -> None:
+        pass
